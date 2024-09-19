cfec55650e2c77e574da23811f37a830466750bb
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-19 10:51:00 +0200

    python

--- PYTHON DATATYPES/OPERATORS/app.py
+++ PYTHON DATATYPES/OPERATORS/app.py
@@ -56,3 +56,12 @@ or	Returns True if one of the statements is true	x < 5 or x < 4
 not	Reverse the result, returns False if the result is true	not(x < 5 and x < 10)
 
 """
+
+"""""
+Python Membership Operators
+Membership operators are used to test if a sequence is presented in an object:
+
+Operator	Description	Example	Try it
+in 	Returns True if a sequence with the specified value is present in the object	x in y	
+not in	Returns True if a sequence with the specified value is not present in the object	x not in y
+"""
