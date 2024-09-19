f135c5706a0368cd4075d7fbe12375289999bf2b
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-19 10:46:30 +0200

    python

---
+++ PYTHON DATATYPES/OPERATORS/app.py
@@ -0,0 +1,16 @@
+# operators
+
+"""""
+Python divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+"""""
+x=1
+y= x*2
+result= x+y
