31cd06adebd31a5c242ec25ef388f1d031bcfc54
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-19 10:50:11 +0200

    python

--- PYTHON DATATYPES/OPERATORS/app.py
+++ PYTHON DATATYPES/OPERATORS/app.py
@@ -45,3 +45,14 @@ Operator	Example	Same As
 :=	print(x := 3)	x = 3
 print(x)
 """""
+"""""
+
+Python Logical Operators
+Logical operators are used to combine conditional statements:
+
+Operator	Description	Example	Try it
+and 	Returns True if both statements are true	x < 5 and  x < 10	
+or	Returns True if one of the statements is true	x < 5 or x < 4	
+not	Reverse the result, returns False if the result is true	not(x < 5 and x < 10)
+
+"""
