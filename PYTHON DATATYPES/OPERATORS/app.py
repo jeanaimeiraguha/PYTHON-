71bdae1b4f32580015730a84134781f67779fc70
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-19 10:47:54 +0200

    python

--- PYTHON DATATYPES/OPERATORS/app.py
+++ PYTHON DATATYPES/OPERATORS/app.py
@@ -11,6 +11,18 @@ Identity operators
 Membership operators
 Bitwise operators
 """""
+# eg:
+"""""
+Arithmetic Operator
+
++	Addition	x + y	
+-	Subtraction	x - y	
+*	Multiplication	x * y	
+/	Division	x / y	
+%	Modulus	x % y	
+**	Exponentiation	x ** y	
+//	Floor division	x // y
+"""""
 x=1
 y= x*2
 result= x+y
