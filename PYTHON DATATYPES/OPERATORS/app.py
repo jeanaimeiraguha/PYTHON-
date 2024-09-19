d230505a669055715f56ffb246614c0325f60cdc
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-19 10:51:58 +0200

    python

--- PYTHON DATATYPES/OPERATORS/app.py
+++ PYTHON DATATYPES/OPERATORS/app.py
@@ -65,3 +65,17 @@ Operator	Description	Example	Try it
 in 	Returns True if a sequence with the specified value is present in the object	x in y	
 not in	Returns True if a sequence with the specified value is not present in the object	x not in y
 """
+
+"""""
+not in	Returns True if a sequence with the specified value is not present in the object	x not in y	
+Python Bitwise Operators
+Bitwise operators are used to compare (binary) numbers:
+
+Operator	Name	Description	Example	Try it
+& 	AND	Sets each bit to 1 if both bits are 1	x & y	
+|	OR	Sets each bit to 1 if one of two bits is 1	x | y	
+^	XOR	Sets each bit to 1 if only one of two bits is 1	x ^ y	
+~	NOT	Inverts all the bits	~x	
+<<	Zero fill left shift	Shift left by pushing zeros in from the right and let the leftmost bits fall off	x << 2	
+>>	Signed right shift	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off	x >> 2
+"""
