4a06e9c179d051be8a5453655455145b17169fd8
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-26 09:36:52 +0200

    python

--- PYTHON DATATYPES/PYTHON LIST/app.py
+++ PYTHON DATATYPES/PYTHON LIST/app.py
@@ -1,8 +1,9 @@
-mylist=["Iraguha" , "Cyusa"]
+mylist=["Iraguha" ,"Cyusa"]
 print(mylist)
 # accessing 
-print(mylist[0])
+print(mylist[-1])
 # changing item value via lists
 mylist[1] = "NBA"
 # Lets access our list
 print(mylist)
+# 
