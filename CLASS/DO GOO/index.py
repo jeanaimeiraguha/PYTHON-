c963dcc0ec4acf5e788edf3b8ab7d69f87f280af
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-26 10:07:13 +0200

    initial commit

---
+++ CLASS/DO GOO/index.py
@@ -0,0 +1,17 @@
+
+
+product = input("Enter your product mehnn so as we go  verify:")
+
+product=[
+    
+        "mango"
+        , "orange"
+    
+]
+if "mango" in product:
+    print("product exist")
+elif "orange " in product:
+    print("orange already exists")
+else:
+    print("nothing to show")
+    
