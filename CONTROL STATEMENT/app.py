6fd998072f1d38a0be9143634dd3c74b3c7aac9d
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-10-10 10:10:18 +0200

    python

--- CONTROL STATEMENT/app.py
+++ CONTROL STATEMENT/app.py
@@ -21,12 +21,19 @@ else:
 #lets check the if marks deserves to be competent
 
 
-marks=100
-if marks>70:
+marks=59
+if marks>=90:
     print('Grade A')
-elif marks>=60:
+elif marks>=80:
     print("Grade B")
- 
+    
+elif marks>=70:
+    print("Grade C")
+    
+elif marks>=60:
+    print("Grade D")
+elif marks>=50:
+    print("Grade E")
     
 else:
     print("failed ")
