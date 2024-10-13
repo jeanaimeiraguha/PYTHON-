71d119540059fde729b05cc281c94c160d2553b1
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-10-13 18:03:26 +0200

    python class

---
+++ VARIABLES NAMINGS/CLASS/class.py
@@ -0,0 +1,9 @@
+class Person:
+    def __int__(self, firstName ,lastName, age,address) :
+
+       self.firstName =firstName
+       self.lastName =lastName
+       self.firstName =firstName
+       self.age =age
+       self.address =address
+       result=Person("Iraguha", "Jean Aime",19,"Nyamata")
