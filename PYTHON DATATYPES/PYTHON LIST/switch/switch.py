c71a56fc49693f870203d8170e038c9ee07b1565
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-10-13 19:15:20 +0200

    switch stmt

---
+++ PYTHON DATATYPES/PYTHON LIST/switch/switch.py
@@ -0,0 +1,16 @@
+lang =input("enter Your favorite language:")
+match lang:
+    case "Javascript":
+        print("You are Amazing")
+    case "Nodejs":
+        print("Good work")
+    case "MongoDb":
+        print("Are you Familiar with NOsql")
+    case "PHP":
+        print("This is old lang")
+    case "PYTHON":
+        print("you are now in the right way to become AI dev")
+    case "solidarity":
+        print("Block chain dev")
+    case _:
+        print("Language entered not found")
