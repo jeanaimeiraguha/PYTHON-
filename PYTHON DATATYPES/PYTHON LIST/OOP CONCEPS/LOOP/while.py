991bc32d6b30508329e08e558863c26185bd2fef
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-10-13 19:27:49 +0200

    python while

--- PYTHON DATATYPES/PYTHON LIST/OOP CONCEPS/LOOP/while.py
+++ PYTHON DATATYPES/PYTHON LIST/OOP CONCEPS/LOOP/while.py
@@ -1,7 +1,17 @@
-
+"""
 #Simple example while loop
 
 count=1
 while count<10:
     print(count)
     count=count -1
+
+"""
+number=int(input("Enter Any number in range between 100 and 500:"))
+ #number=int(input("Enter Any number in range between 100 and 500"))
+while number<100 or number >500:
+    print("Incorect Number , Please enter correct Number")
+   
+else:
+    print("Given Number is correct", number)
+    
