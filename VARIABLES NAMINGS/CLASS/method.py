4dc582ab6b036232414f14b2e4b80c3c8f7adb40
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-10-13 18:28:49 +0200

    methods

--- VARIABLES NAMINGS/CLASS/method.py
+++ VARIABLES NAMINGS/CLASS/method.py
@@ -1,3 +1,5 @@
+"""
+
 class Person:
     def __init__(self, firstName ,lastName, age,address):
         self.firstName=firstName
@@ -7,3 +9,20 @@ class Person:
         # method 
         def mfunc(self):
             print("Welcome ") , self.firstName, self.age
+            #object class
+            p1=Person("Iraguha","Jean Aime" ,39)
+            #function call
+            p1.myfunc()
+            """
+class Person:
+  def __init__(self, name1, lname , age):
+    self.name1 = name1
+    self.age = age
+    self.lname = lname
+  #method definition
+  def myfunc(self):
+    print("Hello my name is " + self.name1 + self.lname)
+#object of class
+p1 = Person("John","kubwimana", 36)
+#function calling
+p1.myfunc()
