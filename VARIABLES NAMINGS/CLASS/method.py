6054eb2fe917d0f8524cecde6337f0245b5868ca
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-10-13 18:44:07 +0200

    methods

--- VARIABLES NAMINGS/CLASS/method.py
+++ VARIABLES NAMINGS/CLASS/method.py
@@ -13,7 +13,7 @@ class Person:
             p1=Person("Iraguha","Jean Aime" ,39)
             #function call
             p1.myfunc()
-            """
+        """    """
 class Person:
   def __init__(self, name1, lname , age):
     self.name1 = name1
@@ -23,6 +23,29 @@ class Person:
   def myfunc(self):
     print("Hello my name is " + self.name1 + self.lname)
 #object of class
-p1 = Person("John","kubwimana", 36)
+p1 = Person("Iraguha","Jean AIme", 19)
 #function calling
 p1.myfunc()
+"""
+"""
+class Animals:
+   def __init__(self, an):
+      self.an=an
+      def myfunc(self):
+      
+         p1=Animals("cow")
+         print("Welcome" + an)
+         p1.myfunc()
+
+"""
+class Animal:  
+    def speak(self):  
+        print("Animal Speaking")  
+#child class Dog inherits the base class Animal  
+class Dog(Animal):  
+    def bark(self):  
+        print("dog barking")
+#object definition  
+obj = Dog()  
+obj.bark()  
+obj.speak() 
